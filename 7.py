715edb152c6d878d4f710571ec9e59cf89523a72
Ivan Galuzin <galuzin627@gmail.com>
2023-04-11 21:39:47 +0700

    3

--- 7.py
+++ 7.py
@@ -9,3 +9,10 @@
 # Input: 2016
 # Output: YES
 
+year = int(input("Введите год: "))
+if (year % 4 == 0 and year % 100 != 0):
+    print("YES")
+elif (year % 400 == 0):
+    print("YES")
+else:
+    print("NO")
