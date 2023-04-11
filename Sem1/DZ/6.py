64fdbfb2cf7ac4fbdcd7ff4f4510e144b01fa964
Ivan Galuzin <galuzin627@gmail.com>
2023-04-11 22:14:59 +0700

    Задача 6

--- Sem1/DZ/6.py
+++ Sem1/DZ/6.py
@@ -6,3 +6,22 @@
 # программу, которая проверяет счастливость билета.
 # 385916 -> yes
 # 123456 -> no
+
+a = int(input("Введите число: "))
+a1 = int(a/100000)
+a2 = int(a/10000%10)
+a3 = int(a/1000%10)
+a4 = int(a/100%10)
+a5 = int(a/10%10)
+a6 = int(a%10)
+aSum1 = int(a1+a2+a3)
+aSum2 = int(a4+a5+a6)
+print(f"{a1} + {a2} + {a3} = {a4} + {a5} + {a6}")
+print(f"{aSum1} = {aSum2}")
+if (a > 99999 and a < 1000000):
+    if(aSum1 == aSum2):
+        print(f"Да")
+    else:
+        print("Нет")
+else:
+    print("Введите шестизначное число!")
