228f3e3397483f5b0fa52eeab388a4014b53611e
Ivan Galuzin <galuzin627@gmail.com>
2023-04-23 23:47:39 +0700

    441

--- Sem3/21.py
+++ Sem3/21.py
@@ -4,3 +4,13 @@
 # {"VI": "S005"}, {"VII": " S005 "}, {" V ":" S009 "}, {" VIII
 # ":" S007 "}]
 # Output: {'S005', 'S002', 'S007', 'S001', 'S009'}
+
+
+test_list = {"V": "S001", "V": "S002", "VI": "S001", "VI": "S005", "VII": " S005 ", " V ":" S009 ", " VIII ":" S007 ", "IV":"holla", "1234": "S005"}
+#res = list(set(val for dic in test_list for val in dic.values()))
+a = set()
+
+for value in test_list.values():
+    a.add(value)
+
+print(a)
--- Sem3/23.py
+++ Sem3/23.py
@@ -4,3 +4,12 @@
 # с предыдущим номером)
 # Input: [0, -1, 5, 2, 3]
 # Output: 2 (-1 < 5, 2 < 3)
+
+arr = [0, -1, 5, 2, 3, 4, 0]
+count = 0
+
+for x in range(1, len(arr)):
+    if arr[x - 1] < arr[x]:
+        count += 1
+
+print(count)
